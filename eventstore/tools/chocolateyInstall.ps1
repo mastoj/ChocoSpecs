245a7b19a37d821a2a64a935423296e3da0feac7
Tomas Jansson <tomas.jansson@gmail.com>
2014-10-04 22:08:20 +0200

    added v3.0

--- eventstore/tools/chocolateyInstall.ps1
+++ eventstore/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$packageName = 'eventstore' 
-$url = 'http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.0.0-rc9.zip'
+$url = 'http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.0.0.zip'
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
