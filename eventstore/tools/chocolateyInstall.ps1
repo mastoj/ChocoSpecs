60d8987b14eb58cfb714f6d28ce5784d3d196c43
Tomas Jansson <tomas.jansson@gmail.com>
2015-05-28 10:37:41 +0200

    updated version of eventstore 3.0.0

--- eventstore/tools/chocolateyInstall.ps1
+++ eventstore/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
 $url = 'http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.0.0.zip'
 
 $programData = $env:ProgramData
-$installFolder = "$programData\eventstore"
+$installFolder = "$programData\eventstore.3.0.0"
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$installFolder"
--- eventstore/tools/chocolateyUninstall.ps1
+++ eventstore/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
 $programData = $env:ProgramData
-$installFolder = "$programData\eventstore"
+$installFolder = "$programData\eventstore.3.0.0"
 Write-Host "Removing folder $installFolder"
 Remove-Item $installFolder -force -recurse
